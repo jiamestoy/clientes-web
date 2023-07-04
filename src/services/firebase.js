d3cf4a5ea7341b1b1630a2478fb70a3c2a672cce
jiamestoy <112114806+jiamestoy@users.noreply.github.com>
2023-07-04 18:42:42 -0300

    nueva base de datos

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyApk13ambUZddFSI2oqcMWOcfpbax7L4-U",
-    authDomain: "clientes-web-tp1.firebaseapp.com",
-    projectId: "clientes-web-tp1",
-    storageBucket: "clientes-web-tp1.appspot.com",
-    messagingSenderId: "405636253095",
-    appId: "1:405636253095:web:a1bc1bf9886c985191e282"
+    apiKey: "AIzaSyBcYLXPt-_QPdzBmLiNKW8fFjzvA4z_EqY",
+    authDomain: "clientes-web-1a6a1.firebaseapp.com",
+    projectId: "clientes-web-1a6a1",
+    storageBucket: "clientes-web-1a6a1.appspot.com",
+    messagingSenderId: "182904982175",
+    appId: "1:182904982175:web:56900f984258ec2557b0aa"
 };
 
 const app = initializeApp(firebaseConfig);
